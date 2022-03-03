3edc29761b48df6ee9d8be83b7bf99713344b0b8
Son La <shiloh1712@gmail.com>
2022-03-03 07:43:07 -0600

    sql review&feedback

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,39 +1,64 @@
 --Son begin
+														--1
 CREATE SCHEMA PO;
-GO
 
---CREATE ENTITY PACKAGE
+--CREATE ENTITY ADDRESS									--2
+CREATE TABLE ADDRESS(
+	AddrID INT PRIMARY KEY IDENTITY(1,1),
+	StreetAddress VARCHAR(30),
+	City VARCHAR(10),
+	StateName CHAR(2),
+	Zipcode CHAR(5)
+)
+
+--TRY INSERTING A PKG
+INSERT INTO ADDRESS (StreetAddress, City, StateName, Zipcode) VALUES ('4800 Calhoun Rd', 'Houston', 'TX', 77024)
+
+--DISPLAY ALL PKG
+select * from ADDRESS
+
+
+--order of create table: address, customer, package, STORE, EMPLOYEE (ADD REFERENCE FROM STORE), TRACKING_RECORD
+--add foreign key for store								--7
+ALTER TABLE STORE
+ADD FOREIGN KEY (Supervisor) REFERENCES	EMPLOYEE(ID)
+
+--CREATE ENTITY PACKAGE									--4
 CREATE TABLE PACKAGE(
 	TrackingNumber char(12) NOT NULL PRIMARY KEY,
-	Sender int NOT NULL,
-	Recipient INT NOT NULL,
-	ToAddress int NOT NULL,
-	Descrp varchar(60),
+	Sender int NOT NULL, --reference customer
+	Recipient INT NOT NULL,	--reference customer
+	ToAddress int NOT NULL,	--reference address
+	Descrp varchar(60),	
 	Stat tinyint DEFAULT 0,
 	TimeDelivered datetime,
 	CHECK (Stat <= 5 and Stat >=0)
 )
-GO
+
 --TRY INSERTING A PKG
 INSERT INTO PACKAGE (TrackingNumber,Sender,	Recipient,ToAddress,Descrp) VALUES (123456789012, 1, 2, 3, 'SOME STUFF' )
-GO
+
 --DISPLAY ALL PKG
 select * from PACKAGE
-GO
+
+
+
 --Son end
 
---Andy begin
+--Andy begin											--6
 CREATE TABLE EMPLOYEE(
-	ID int NOT NULL,
+	ID int NOT NULL, 
 	Employee_Name varchar(15) NOT NULL,
 	Phone_Number char(10) NOT NULL,
 	Address int NOT NULL, /* Address is highlighted blue, will that be ok? Or does it need a name change*/
 	Password varchar(20) NOT NULL, /*dont know how to set parameters for varchar, maybe put input restrictions*/
 	Work_Place int NOT NULL
-
+	--missing PK; 
+	--use other attribute names for address+password; 
+	--Work_place,Address need to reference STORE, ADDRESS tables
 );
 
---Morrison begin
+--Morrison begin										--8
 CREATE TABLE TRACKING_RECORD(
 	ID INT NOT NULL,
 	Employee_ID INT NOT NULL,
@@ -43,19 +68,28 @@ CREATE TABLE TRACKING_RECORD(
 	Time_Out DATETIME,
 	Address_ID INT NOT NULL,
 	PRIMARY KEY (ID), 
-	FOREIGN KEY (Address_ID) REFERENCES PACKAGE(ToAddress)
+	FOREIGN KEY (Address_ID) REFERENCES PACKAGE(ToAddress) 
+	/*
+	address_id actually reference address table, not package
+	Employee_ID, Tracking_Number, Store_ID need to reference EMPLOYEE, PACKAGE, STORE table
+	*/
  );
 
 
---Bader begin
+--Bader begin											--5
 CREATE TABLE STORE(
 	storeID int NOT NULL  
 	, Supervisor int NOT NULL
 	, Address int NOT NULL
 	, Register ()
+	/*
+	missing PK
+	Supervisor (Son did this), Address need to reference EMPLOYEE, ADDRESS tables
+	Register ()?
+	*/
 );
 
---Josh Begin
+--Josh Begin											--3
 CREATE TABLE CUSTOMER(
 	ID int PRIMARY KEY NOT NULL,
 	custName varchar(15) NOT NULL,
@@ -63,4 +97,7 @@ CREATE TABLE CUSTOMER(
 	email varchar(20) NOT NULL,
 	pswrd varchar(20) CONSTRAINT CK_Users_Pswrd CHECK (LEN(Pswrd) >= 8),
 	addr int NOT NULL,
+	/*
+	addr need to refenrece ADDRESS TABLE
+	*/
 	)
