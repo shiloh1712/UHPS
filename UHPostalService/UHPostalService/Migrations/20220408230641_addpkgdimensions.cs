4f89c9f24144c5a918cbef09c7bb9d448dfcfec3
Son La <shiloh1712@gmail.com>
2022-04-08 18:07:45 -0500

    add pkg dimensions

---
+++ UHPostalService/UHPostalService/Migrations/20220408230641_addpkgdimensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UHPostalService.Migrations
+{
+    public partial class addpkgdimensions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Depth",
+                table: "Packages",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Height",
+                table: "Packages",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Width",
+                table: "Packages",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Depth",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "Packages");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "Packages");
+        }
+    }
+}
